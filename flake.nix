d1ad4e096c59414a2eca2983e5b24ff71e631d94
gamoutatsumi <tatsumi@gamou.dev>
2025-04-13 12:37:51 +0900

    inital commit

---
+++ flake.nix
@@ -0,0 +1,188 @@
+{
+  description = "Default Flake";
+
+  inputs = {
+    # keep-sorted start block=yes
+    fenix = {
+      url = "https://flakehub.com/f/nix-community/fenix/0.1.*";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+    flake-checker = {
+      url = "github:DeterminateSystems/flake-checker";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+        fenix = {
+          follows = "fenix";
+        };
+        naersk = {
+          follows = "naersk";
+        };
+      };
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs = {
+        nixpkgs-lib = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+    naersk = {
+      url = "https://flakehub.com/f/nix-community/naersk/0.1.*";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+    nixpkgs-unstable = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs-unstable";
+        };
+        flake-compat = {
+          follows = "flake-compat";
+        };
+      };
+    };
+    systems = {
+      url = "github:nix-systems/default";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+    # keep-sorted end
+  };
+
+  outputs =
+    {
+      flake-parts,
+      systems,
+      flake-checker,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      {
+        inputs,
+        lib,
+        ...
+      }:
+      {
+        systems = import systems;
+        imports =
+          [ flake-parts.flakeModules.easyOverlay ]
+          ++ lib.optionals (inputs.pre-commit-hooks ? flakeModule) [ inputs.pre-commit-hooks.flakeModule ]
+          ++ lib.optionals (inputs.treefmt-nix ? flakeModule) [ inputs.treefmt-nix.flakeModule ];
+
+        perSystem =
+          {
+            system,
+            pkgs,
+            config,
+            ...
+          }:
+          let
+            treefmtBuild = config.treefmt.build;
+          in
+          {
+            _module = {
+              args = {
+                pkgs = import inputs.nixpkgs-unstable {
+                  inherit system;
+                  config = {
+                    allowUnfree = true;
+                  };
+                };
+              };
+            };
+            checks = config.packages;
+            devShells = {
+              default = pkgs.mkShell {
+                PFPATH = "${
+                  pkgs.buildEnv {
+                    name = "zsh-comp";
+                    paths = config.devShells.default.nativeBuildInputs;
+                    pathsToLink = [ "/share/zsh" ];
+                  }
+                }/share/zsh/site-functions";
+                packages = with pkgs; [
+                  nil
+                  efm-langserver
+                ];
+                inputsFrom =
+                  lib.optionals (inputs.pre-commit-hooks ? flakeModule) [ config.pre-commit.devShell ]
+                  ++ lib.optionals (inputs.treefmt-nix ? flakeModule) [ treefmtBuild.devShell ];
+              };
+            };
+          }
+          // lib.optionalAttrs (inputs.pre-commit-hooks ? flakeModule) {
+            pre-commit = {
+              check = {
+                enable = true;
+              };
+              settings = {
+                src = ./.;
+                hooks = {
+                  # keep-sorted start block=yes
+                  flake-checker = lib.optionalAttrs (inputs.flake-checker ? packages) {
+                    enable = true;
+                    package = flake-checker.packages.${system}.flake-checker;
+                  };
+                  treefmt = {
+                    enable = true;
+                    packageOverrides = {
+                      treefmt = treefmtBuild.wrapper;
+                    };
+                  };
+                  # keep-sorted end
+                };
+              };
+            };
+          }
+          // lib.optionalAttrs (inputs.treefmt-nix ? flakeModule) {
+            formatter = treefmtBuild.wrapper;
+            treefmt = {
+              projectRootFile = "flake.nix";
+              flakeCheck = false;
+              programs = {
+                # keep-sorted start block=yes
+                deadnix = {
+                  enable = true;
+                };
+                keep-sorted = {
+                  enable = true;
+                };
+                nixfmt = {
+                  enable = true;
+                };
+                statix = {
+                  enable = true;
+                };
+                # keep-sorted end
+              };
+            };
+          };
+      }
+    );
+}
